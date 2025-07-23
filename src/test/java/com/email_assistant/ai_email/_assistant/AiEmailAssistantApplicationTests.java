4de7298e7ee1d1c8e2b4cf160f0553d97165ca67
divyanshipathak01 <pathakdivyanshi0206@gmail.com>
2025-07-23 23:24:13 +0530

    added

---
+++ src/test/java/com/email_assistant/ai_email/_assistant/AiEmailAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email_assistant.ai_email._assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
